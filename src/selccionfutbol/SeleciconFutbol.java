7844b521d296b4017ac9d9b77cfaeda424c75eed
dariioosm <dariosanchez351@gmail.com>
2024-05-09 12:45:59 +0200

    creacion de los metodos del abstract

--- src/selccionfutbol/SeleciconFutbol.java
+++ src/selccionfutbol/SeleciconFutbol.java
@@ -1,5 +1,19 @@
 package selccionfutbol;
 
-public class SeleciconFutbol {
+public abstract class SeleciconFutbol {
+    protected String nombre,apellidos;
+    protected int id,edad;
     
+    public void viajar(){
+        System.out.println("Viajar clase padre");
+    }
+
+    public void concentrarse(){
+        System.out.println("Concentrarse clase padre");
+    }
+    public abstract void entrenamiento();
+
+    public void partidoFutbol(){
+        System.out.println("Asiste al partido de futbol clase padre");
+    }
 }

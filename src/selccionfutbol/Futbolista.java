101162db8e50fb7b4fadf31354113bdfa530c909
dariioosm <dariosanchez351@gmail.com>
2024-05-09 12:50:42 +0200

    creacion de metodos , getter y setter de Futbolista

--- src/selccionfutbol/Futbolista.java
+++ src/selccionfutbol/Futbolista.java
@@ -1,5 +1,33 @@
 package selccionfutbol;
 
-public class Futbolista {
+
+public class Futbolista extends SeleciconFutbol    {
+    private int dorsal;
+    private String  demarcacion;
+
+    public int getDorsal() {
+        return dorsal;
+    }
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+    public void setDemarcacion(String demarcacion) {
+        this.demarcacion = demarcacion;
+    }
+    @Override
+    public void entrenamiento(){
+        System.out.println("Realiza un entrenamiento clase futbolista");
+    }
+    @Override
+    public void partidoFutbol(){
+        System.out.println("Juega partido de futbol clase futbolista");
+    }
     
+    @Override
+    public void entrevista(){
+        System.out.println("Da una entrevista");
+    }   
 }

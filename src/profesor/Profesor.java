765e291820923fb10dba9252af3b4b3b46c9af52
dariioosm <dariosanchez351@gmail.com>
2024-05-09 13:34:19 +0200

    continuacion de problema profesor

--- src/profesor/Profesor.java
+++ src/profesor/Profesor.java
@@ -5,4 +5,41 @@ public abstract class Profesor {
     private String apellidos;
     private int edad;
     private String IdProfesor;
+    
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellidos() {
+        return apellidos;
+    }
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getIdProfesor() {
+        return IdProfesor;
+    }
+    public void setIdProfesor(String idProfesor) {
+        IdProfesor = idProfesor;
+    }
+
+    public Profesor(String nombre, String apellidos, int edad, String idProfesor) {
+        super();
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.IdProfesor = idProfesor;
+    }
+
+    public void importeNomina(){}
+
+
 }

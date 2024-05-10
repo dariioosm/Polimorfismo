5f199336ffb686d0e169a9241a4309ca9fe331ef
dariioosm <dariosanchez351@gmail.com>
2024-05-10 10:18:59 +0200

    comienzo de nomina

--- src/profesor/ProfesorTitular.java
+++ src/profesor/ProfesorTitular.java
@@ -1,6 +1,23 @@
 package profesor;
 
 public class ProfesorTitular extends Profesor {
-    private double complemento;
-    private int trienio;
+    private double complementoDestino;
+    private int [] trienio;
+
+    public double getComplementoDestino() {
+        return complementoDestino;
+    }
+    public void setComplementoDestino(double complementoDestino) {
+        this.complementoDestino = complementoDestino;
+    }
+    public int[] getTrienio() {
+        return trienio;
+    }
+    public void setTrienio(int[] trienio) {
+        this.trienio = trienio;
+    }
+
+
+
+    
 }

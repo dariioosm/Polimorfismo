32a0eb6fa48dd295b3adf6d8ebfebc4f0f205968
dariioosm <dariosanchez351@gmail.com>
2024-05-11 10:16:07 +0200

    continuacion de profesor titular

--- src/profesor/ProfesorTitular.java
+++ src/profesor/ProfesorTitular.java
@@ -2,7 +2,7 @@ package profesor;
 
 public class ProfesorTitular extends Profesor {
     private double complementoDestino;
-    private int[] trienio;
+    private int trienio;
 
     public double getComplementoDestino() {
         return complementoDestino;
@@ -12,12 +12,27 @@ public class ProfesorTitular extends Profesor {
         this.complementoDestino = complementoDestino;
     }
 
-    public int[] getTrienio() {
+    public int getTrienio() {
         return trienio;
     }
 
-    public void setTrienio(int[] trienio) {
+    public void setTrienio(int trienio) {
         this.trienio = trienio;
+    }
+
+    public ProfesorTitular(String nombre, String apellidos, int edad, String idProfesor, double sueldo,double complementoDestino, int trienio){
+        super(nombre, apellidos, edad, idProfesor, sueldo);
+        this.complementoDestino=complementoDestino;
+        this.trienio=trienio;
+
+    }
+
+
+    @Override
+    public void importeNomina() {
+        double mensualidad = 30 * 43.20;
+        System.out.println("El sueldo sin trienios es: " + mensualidad);
+
     }
 
 }

4221c90d7654b94ff256379a9a98751372903719
dariioosm <dariosanchez351@gmail.com>
2024-05-12 11:15:37 +0200

    correccion del metodo importeNomina para la clase titular

--- src/profesor/ProfesorTitular.java
+++ src/profesor/ProfesorTitular.java
@@ -29,8 +29,15 @@ public class ProfesorTitular extends Profesor {
     }
 
     public void importeTotal(int trienio) {
+
+        System.out.println();
+    }
+
+    @Override
+    public void importeNomina() {
         int[] anno = { 1, 2, 3, 4, 5, 6 };
         double[] prima = { 1.01, 1.02, 1.05, 1.06, 1.07, 1.08 };
+        double mensualidad = 30 * 43.20;
         double total = 0;
         double todo = total + getComplementoDestino();
         for (int i = 0; i < anno.length; i++) {
@@ -38,13 +45,8 @@ public class ProfesorTitular extends Profesor {
                 total = (super.getSueldo() * prima[i]) + super.getSueldo();
             }
         }
-        System.out.println("Sueldo con trienio incluido: " + total + "\n" + "Sueldo con todos los extras: " + todo);
-    }
-
-    @Override
-    public void importeNomina() {
-        double mensualidad = 30 * 43.20;
-        System.out.println("El sueldo sin trienios es: " + mensualidad);
+        System.out.println("El sueldo sin trienios es: " + mensualidad + "\n" + "Sueldo con trienio incluido: " + total
+                + "\n" + "Sueldo con todos los extras: " + todo);
 
     }
 }

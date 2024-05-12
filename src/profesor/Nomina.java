8bebb98d4062b3f450ae9e3d00c6464eb7fd8a52
dariioosm <dariosanchez351@gmail.com>
2024-05-12 11:35:38 +0200

    aclaracion de la clase Nomina

--- src/profesor/Nomina.java
+++ src/profesor/Nomina.java
@@ -1,5 +1,9 @@
 package profesor;
 
 public class Nomina {
-
+    /*
+     * Grabar en un fichero el IdProfesor y su nómina, para lo cual se debe crear un
+     * ArrayList de la
+     * clase nómina.
+     */
 }

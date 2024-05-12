63d694ada0e22c8ee98eb7a33b7bad9d06d98d47
dariioosm <dariosanchez351@gmail.com>
2024-05-12 17:34:22 +0200

    correcciones

--- src/profesor/ProfesorInterino.java
+++ src/profesor/ProfesorInterino.java
@@ -2,6 +2,10 @@ package profesor;
 
 public class ProfesorInterino extends Profesor {
 
+    private static final int dias = 30;
+    private static final int extras = 600;
+    private static final double saldo = 35.60;
+
     public ProfesorInterino(String nombre, String apellidos, int edad, String idProfesor, double sueldo) {
         super(nombre, apellidos, edad, idProfesor, sueldo);
 
@@ -9,8 +13,8 @@ public class ProfesorInterino extends Profesor {
 
     @Override
     public void importeNomina() {
-        double mensualidad = 30 * 35.60;
-        double total = mensualidad + 600;
+        double mensualidad = dias * saldo;
+        double total = mensualidad + extras;
         System.out.println("El sueldo del profesor interino sin extras es: " + mensualidad + "\n"
                 + "El sueldo del interino incuyendo extras es: " + total);
     }

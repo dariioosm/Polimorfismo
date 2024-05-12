cdcde0762153943831c6e866269164108c38ac4b
dariioosm <dariosanchez351@gmail.com>
2024-05-12 17:04:56 +0200

    testing test

--- src/profesor/Test.java
+++ src/profesor/Test.java
@@ -3,6 +3,9 @@ package profesor;
 import java.util.ArrayList;
 
 public class Test {
+    ArrayList<ProfesorInterino> colegio1 = new ArrayList<ProfesorInterino>();
+    ArrayList<ProfesorTitular> colegio = new ArrayList<ProfesorTitular>();
+
     public void cargaArray() {
         ProfesorInterino pi1 = new ProfesorInterino("José", "Hernández López", 0, "45221887-K", 1789);
         ProfesorInterino pi2 = new ProfesorInterino("Andrés", "Moltó Parra", 0, "72332634-L", 1200);
@@ -10,20 +13,27 @@ public class Test {
         ProfesorTitular pt1 = new ProfesorTitular("Juan", "Pérez Pérez", 0, "73-K", 1900, 2, 0);
         ProfesorTitular pt2 = new ProfesorTitular("Alberto", "Centa Mota", 0, "88-L", 1800, 3, 0);
         ProfesorTitular pt3 = new ProfesorTitular("Maria", "Mota Pérez", 0, "81-F", 1700, 5, 0);
-        ArrayList<ProfesorInterino> colegio1 = new ArrayList<ProfesorInterino>();
-        ArrayList<ProfesorTitular> colegio = new ArrayList<ProfesorTitular>();
-       
-        public void imprimeArray(){
-            for (ProfesorTitular profesorTitular : colegio) {
-                System.out.println(profesorTitular);
-            }
-        
+
+        colegio1.add(pi1);
+        colegio1.add(pi2);
+        colegio1.add(pi3);
+        colegio.add(pt1);
+        colegio.add(pt2);
+        colegio.add(pt3);
     }
-  
+
     public static void main(String[] args) {
-        
-    imprimeArray();
-    
-}
-}
+        Test test = new Test(); // Create an instance of Test class
+        test.cargaArray(); // Call cargaArray method to populate the ArrayLists
+
+        System.out.println("Profesor Interino:");
+        for (ProfesorInterino interino : test.colegio1) {
+            System.out.println(interino);
+        }
+
+        System.out.println("Profesor Titular:");
+        for (ProfesorTitular titular : test.colegio) {
+            System.out.println(titular);
+        }
+    }
 }

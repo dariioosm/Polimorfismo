8fbc40bb68751ddeae7ae25eaf06f2caab8c92a2
dariioosm <dariosanchez351@gmail.com>
2024-05-12 12:11:37 +0200

    prueba del main

--- src/profesor/Test.java
+++ src/profesor/Test.java
@@ -3,9 +3,6 @@ package profesor;
 import java.util.ArrayList;
 
 public class Test {
-    ArrayList<ProfesorInterino> colegio1 = new ArrayList<ProfesorInterino>();
-    ArrayList<ProfesorTitular> colegio = new ArrayList<ProfesorTitular>();
-
     public void cargaArray() {
         ProfesorInterino pi1 = new ProfesorInterino("José", "Hernández López", 0, "45221887-K", 1789);
         ProfesorInterino pi2 = new ProfesorInterino("Andrés", "Moltó Parra", 0, "72332634-L", 1200);
@@ -13,7 +10,20 @@ public class Test {
         ProfesorTitular pt1 = new ProfesorTitular("Juan", "Pérez Pérez", 0, "73-K", 1900, 2, 0);
         ProfesorTitular pt2 = new ProfesorTitular("Alberto", "Centa Mota", 0, "88-L", 1800, 3, 0);
         ProfesorTitular pt3 = new ProfesorTitular("Maria", "Mota Pérez", 0, "81-F", 1700, 5, 0);
-
+        ArrayList<ProfesorInterino> colegio1 = new ArrayList<ProfesorInterino>();
+        ArrayList<ProfesorTitular> colegio = new ArrayList<ProfesorTitular>();
+       
+        public void imprimeArray(){
+            for (ProfesorTitular profesorTitular : colegio) {
+                System.out.println(profesorTitular);
+            }
+        
     }
+  
+    public static void main(String[] args) {
+        
+    imprimeArray();
     
+}
+}
 }

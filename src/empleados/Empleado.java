3b3ea4911176814f2dee4168b7bbfe336454b83f
dariioosm <dariosanchez351@gmail.com>
2024-05-14 13:26:00 +0200

    creacion de getter y setter de la clase abstracta

--- src/empleados/Empleado.java
+++ src/empleados/Empleado.java
@@ -6,6 +6,38 @@ public abstract class  Empleado {
     private String apellidos;
     protected int fechaIngreso;
 
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(int fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
     public Empleado(){
         super();
     }
--- src/empleados/EmpleadoContratado.java
+++ src/empleados/EmpleadoContratado.java
@@ -5,6 +5,15 @@ public class EmpleadoContratado extends Empleado {
     
     public void calculaNomina(){
         double[]prima={1.01,1.02,1.05,1.06,1.07,1.08,1.10,1.11,1.12,1.13,1.14,1.15,1.20};
+        double total=0;
+        for (int i =0; i<prima.length){
+            if(prima[i]<12){
+                total=();
+            }
+            else{
+                total=
+            }
+        }
     
     }
    

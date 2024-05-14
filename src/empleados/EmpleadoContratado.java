8b1b79b01c9312ad03ebc0e83434c2a119f388e1
dariioosm <dariosanchez351@gmail.com>
2024-05-14 13:51:44 +0200

    correccion del metodo nomina

--- src/empleados/EmpleadoContratado.java
+++ src/empleados/EmpleadoContratado.java
@@ -2,16 +2,24 @@ package empleados;
 
 public class EmpleadoContratado extends Empleado {
     private float salarioC;
+   
+    /*calcular antiguedad en la empresa*/
     
+
     public void calculaNomina(){
-        double[]prima={1.01,1.02,1.05,1.06,1.07,1.08,1.10,1.11,1.12,1.13,1.14,1.15,1.20};
+        
+        double[]prima={1.01,1.02,1.05,1.06,1.07,1.08,1.10,1.11,1.12,1.13,1.14,1.15};
         double total=0;
-        for (int i =0; i<prima.length){
-            if(prima[i]<12){
-                total=();
+        int  annos = 2024-getFechaIngreso();    
+        if (){
+            
+        }
+        for (int i =0; i<prima.length;i++){
+            if(annos<12){
+                total=(getSalario()*prima[i]);
             }
             else{
-                total=
+                total=(getSalario()*1.20);
             }
         }
     
@@ -19,16 +27,7 @@ public class EmpleadoContratado extends Empleado {
    
    
    
-   
-   
-   
-   
-   
-   
-   
-   
-   
-   
+      
     @Override
     public float getSalario() {
         // TODO Auto-generated method stub

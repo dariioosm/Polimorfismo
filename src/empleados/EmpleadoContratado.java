5426a85f8187de7a1eecee1e4951567480a371a9
dariioosm <dariosanchez351@gmail.com>
2024-05-14 13:53:45 +0200

    correcciones menores

--- src/empleados/EmpleadoContratado.java
+++ src/empleados/EmpleadoContratado.java
@@ -7,7 +7,12 @@ public class EmpleadoContratado extends Empleado {
    
    
    
-      
+    
+    public EmpleadoContratado(String dni, String nombre, String apellidos, int fechaIngreso, float salarioC) {
+        super(dni, nombre, apellidos, fechaIngreso);
+        this.salarioC = salarioC;
+    }
+
     @Override
     public float getSalario() {
         double[]prima={1.01,1.02,1.05,1.06,1.07,1.08,1.10,1.11,1.12,1.13,1.14,1.15};

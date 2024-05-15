ded251f01c84184c0e57b1c1b76de564fc0b7e52
dariioosm <dariosanchez351@gmail.com>
2024-05-15 13:38:09 +0200

    creacion de atributos, getter setter y constructores

--- src/videoclub/Videojuego.java
+++ src/videoclub/Videojuego.java
@@ -1,5 +1,33 @@
 package videoclub;
 
 public class Videojuego {
+    private String titulo;
+    private int horasEstimadas;
+    private boolean entregado;
+
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getHorasEstimadas() {
+        return horasEstimadas;
+    }
+    public void setHorasEstimadas(int horasEstimadas) {
+        this.horasEstimadas = horasEstimadas;
+    }
+    
+
+    public Videojuego(){
+
+    }
+
+    public Videojuego(String titulo, int horasEstimadas){
+        this.titulo=titulo;
+        this.horasEstimadas=10;
+        
+    }
+
     
 }

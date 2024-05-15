8bd38931528fc91e505981308058514c0b9ef75d
dariioosm <dariosanchez351@gmail.com>
2024-05-15 13:23:04 +0200

    comienzo de serie

--- src/videoclub/Entregable.java
+++ src/videoclub/Entregable.java
@@ -1,5 +1,7 @@
 package videoclub;
 
 public interface Entregable {
-
+    public void entregar();
+    public void devolver();
+    public boolean isEntregado();
 }
---
+++ src/videoclub/Serie.java
@@ -0,0 +1,40 @@
+package videoclub;
+
+public class Serie {
+    private String titulo;
+    private int temporadas;
+    private String genero;
+    private String creador;
+    
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getTemporadas() {
+        return temporadas;
+    }
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+    public String getCreador() {
+        return creador;
+    }
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
+
+
+
+
+
+
+    
+}

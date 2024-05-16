74c8b55a3e84c366485932171a0cc091847b14b7
dariioosm <dariosanchez351@gmail.com>
2024-05-16 12:13:13 +0200

    correcciones menore

--- src/llamadas/Llamada.java
+++ src/llamadas/Llamada.java
@@ -5,5 +5,29 @@ public abstract class Llamada {
     private String desetino;
     private int duracion;
 
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDesetino() {
+        return desetino;
+    }
+
+    public void setDesetino(String desetino) {
+        this.desetino = desetino;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
     public abstract int calcularCoste();
 }
---
+++ src/llamadas/LlamadaEstandar.java
@@ -0,0 +1,13 @@
+package llamadas;
+
+public class LlamadaEstandar implements Llamada {
+    
+    
+    
+    @Override
+    private int calcularCoste(){
+        return coste;
+    }
+
+    
+}

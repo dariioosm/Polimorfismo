81d52713767e5ceaf698439b602ed512e4254c48
dariioosm <dariosanchez351@gmail.com>
2024-05-22 13:23:09 +0200

    Creacion de metodos y atributos de la clase VentaOnline

--- src/recorrecciondelesamen/VentaOnline.java
+++ src/recorrecciondelesamen/VentaOnline.java
@@ -1,5 +1,53 @@
 package recorrecciondelesamen;
 
-public class VentaOnline {
-    
+public class VentaOnline extends Venta {
+    protected String metodoPago;
+	protected String direccionEnvio;
+	
+	public String getMetodoPago() {
+		return metodoPago;
+	}
+	public void setMetodoPago(String metodoPago) {
+		this.metodoPago = metodoPago;
+	}
+	public String getDireccionEnvio() {
+		return direccionEnvio;
+	}
+	public void setDireccionEnvio(String direccionEnvio) {
+		this.direccionEnvio = direccionEnvio;
+	}
+	public VentaOnline(String metodoPago, String direccionEnvio) {
+		super();
+		this.metodoPago = metodoPago;
+		this.direccionEnvio = direccionEnvio;
+	}
+	
+	public VentaOnline() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public VentaOnline(String idProducto, double precio, int cantidad, String marca,String metodoPago, String direccionEnvio) {
+		super(idProducto, precio, cantidad, marca);
+		this.metodoPago = metodoPago;
+		this.direccionEnvio = direccionEnvio;
+	}
+	@Override
+	public double beneficio() {
+		 String[] marcasRopa = { "Nike", "Adidas", "Zara", "H&M", "GAP" };
+	        double[] porcentajes_ventaOnline = { 0.11, 0.15, 0.23, 0.25, 0.31 };
+	        double beneficio = 0;
+
+	        for (int i = 0; i < marcasRopa.length; i++) {
+	            if (this.marca.equals(marcasRopa[i])) {
+	                beneficio = this.precio * this.cantidad * porcentajes_ventaOnline[i];
+	            }
+	        }
+
+	        return beneficio;
+	}
+	@Override
+	public String toString() {
+		return "\n"+"VentaOnline "+super.toString()+" metodo de pago"+this.metodoPago+" direccion de envio "+this.direccionEnvio ;
+	}
 }
--- src/recorrecciondelesamen/VentaTienda.java
+++ src/recorrecciondelesamen/VentaTienda.java
@@ -1,5 +1,48 @@
 package recorrecciondelesamen;
 
-public class VentaTienda {
+public class VentaTienda extends Venta {
+
+    protected String vendedor;
+	protected String ubicacion;
+	public String getVendedor() {
+		return vendedor;
+	}
+	public void setVendedor(String vendedor) {
+		this.vendedor = vendedor;
+	}
+	public String getUbicacion() {
+		return ubicacion;
+	}
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	public VentaTienda( String idProducto, double precio, int cantidad, String marca,String vendedor, String ubicacion) {
+		super(idProducto,precio, cantidad,marca);
+		this.vendedor = vendedor;
+		this.ubicacion = ubicacion;
+	}
+	public VentaTienda() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	//metodo abstracto de Venta
+	@Override
+	public double beneficio() {
+        String[] marcasRopa = { "Nike", "Adidas", "Zara", "H&M", "GAP" };
+        double[] porcentajes_ventaLocal = { 0.02,0.05,0.10,0.20,0.28};
+        double beneficio = 0;
+
+        for (int i = 0; i < marcasRopa.length; i++) {
+            if (this.marca.equals(marcasRopa[i])) {
+                beneficio = this.precio * this.cantidad * porcentajes_ventaLocal[i];
+            }
+        }
+        return beneficio;
+	}
+	@Override
+	public String toString() {
+		return "\n"+"VentaLocal "+super.toString()+" vendedor "+this.vendedor+" ubicacion de la tienda "+this.ubicacion; 
+	}
+	
     
 }

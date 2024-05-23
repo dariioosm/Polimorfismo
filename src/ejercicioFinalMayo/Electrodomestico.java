97e5e3d152b6a4859b1bfda687d4c87d6241bf21
dariioosm <dariosanchez351@gmail.com>
2024-05-23 16:33:58 +0200

    comienzo del problema electrodomesticas

---
+++ src/ejercicioFinalMayo/Electrodomestico.java
@@ -0,0 +1,9 @@
+package ejercicioFinalMayo;
+
+public class Electrodomestico {
+    protected double precioBase;
+    protected int peso;
+    protected char consumo;
+    protected String color;
+
+}

a0d93c9853d03832ef10ee25947e3608de429b6e
dariioosm <dariosanchez351@gmail.com>
2024-05-23 14:20:20 +0200

    correcciones menores

--- src/ejerciciosrecuperacionmayo/EjercicioMatriz.java
+++ src/ejerciciosrecuperacionmayo/EjercicioMatriz.java
@@ -30,16 +30,21 @@ public class EjercicioMatriz {
          for(int i=0; i<tabla.length;i++){
             System.out.println();//print vacio para que salte linea
             for(int j=0; j<tabla[0].length;j++){
-                System.out.print(tabla[i][j]+" "); //print que imprime en la misma linea
+                System.out.print("\t"+tabla[i][j]+" "); //print que imprime en la misma linea
             }
             
         }
        }
        //metodo que cuenta multiplos de 5 por columnas
        public void multiCinco(int [][]tabla){
+        int cont=0;
         for (int i=0; i<tabla.length;i++){
             for(int j=0;j<tabla[0].length;j++){
                     if (tabla[i][j]%5==0) {//si el resto es 0 al dividir entre 5
+                        
+                       // cont++;
+                        //System.out.println("\n"+"Hay "+cont+" multiplos de 5 en la posicion Fila/Columna: "+i+j);
+                        System.out.println();
                         System.out.println("El numero "+tabla[i][j]+" que esta en la posicion " +" Fila: "+i+" Columna: " +j+" Es multiplo de 5");
                     }
             }

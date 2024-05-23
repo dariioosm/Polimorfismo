48683d080d15a682118f3708982db758a1c185d0
dariioosm <dariosanchez351@gmail.com>
2024-05-23 16:41:15 +0200

    getter y setter

--- src/ejercicioFinalMayo/Electrodomestico.java
+++ src/ejercicioFinalMayo/Electrodomestico.java
@@ -6,4 +6,36 @@ public class Electrodomestico {
     protected char consumo;
     protected String color;
 
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public char getConsumo() {
+        return consumo;
+    }
+
+    public void setConsumo(char consumo) {
+        this.consumo = consumo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
 }

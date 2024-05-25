5322cfb683ae7e01114efd090dca2359562b10e5
dariioosm <dariosanchez351@gmail.com>
2024-05-25 16:08:49 +0200

    comienzo de la clase de gestion

--- src/ejercicioFinalMayo/ElectrodomesticoAL.java
+++ src/ejercicioFinalMayo/ElectrodomesticoAL.java
@@ -4,4 +4,19 @@ import java.util.*;
 
 public class ElectrodomesticoAL {
     ArrayList<Electrodomestico> ventas = new ArrayList<Electrodomestico>();
+
+    public void cargaLista() {
+        ventas.add(new Television(500, 10, 'E', "negro", 42, false));
+        ventas.add(new Television(200, 20, 'C', "naranja", 30, true));
+        ventas.add(new Lavadora(600, 20, 'D', "gris", 10));
+        ventas.add(new Lavadora(300, 40, 'Z', "blanco", 40));
+        ventas.add(new Lavadora(400, 60, 'A', "verde", 15));
+    }
+
+    public void imprimeLista() {
+        for (Electrodomestico electrodomestico : ventas) {
+            System.out.println(electrodomestico);
+        }
+    }
+
 }

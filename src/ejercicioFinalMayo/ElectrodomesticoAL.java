4f30ad628af036829757748353019dfe1b915294
dariioosm <dariosanchez351@gmail.com>
2024-05-25 16:53:11 +0200

    correccion de ficheros

--- src/ejercicioFinalMayo/ElectrodomesticoAL.java
+++ src/ejercicioFinalMayo/ElectrodomesticoAL.java
@@ -1,5 +1,6 @@
 package ejercicioFinalMayo;
 
+import java.io.*;
 import java.util.*;
 
 public class ElectrodomesticoAL {
@@ -16,6 +17,77 @@ public class ElectrodomesticoAL {
     public void imprimeLista() {
         for (Electrodomestico electrodomestico : ventas) {
             System.out.println(electrodomestico);
+        }
+    }
+
+    public void annadirProducto() {
+        Scanner in = new Scanner(System.in);
+        char continua;
+        do {
+            System.out.println("Introduzca precio base del producto");
+            double precioBase = in.nextDouble();
+            System.out.println("Introduce el peso del producto");
+            int peso = in.nextInt();
+            System.out.println("Introduce el consumo del producto A-F");
+            char letra = in.next().charAt(0);
+            System.out.println("Introduzca el color del producto");
+            String color = in.nextLine();
+            color = in.nextLine();
+            System.out.println("Es una lavadora (1) o una television (2)");
+            int op = in.nextInt();
+            while (op != 1 && op != 2) {
+                System.out.println("Introduce una opcion valida, 1 o 2");
+            }
+            switch (op) {
+                case 1:
+                    System.out.println("Introduce el numero de pulgadas de la TV");
+                    int resolucion = in.nextInt();
+                    System.out.println("Indique si es SmartTv o no");
+                    String smart = in.nextLine();
+                    if (smart.equalsIgnoreCase("si")) {
+                        boolean smartTv = true;
+                    } else if (smart.equalsIgnoreCase("no")) {
+                        boolean smartTv = false;
+                    }
+                    ventas.add(new Television(precioBase, peso, letra, color, resolucion, false));
+                    break;
+
+                case 2:
+                    System.out.println("Introduce carga de la lavadora en kg");
+                    int carga = in.nextInt();
+                    ventas.add(new Lavadora(precioBase, peso, letra, color, carga));
+                    break;
+            }
+            System.out.println("Desea continuar(S/N)");
+            continua = in.next().charAt(0);
+
+        } while (continua == 'S' || continua == 's');
+    }
+
+    public void grabaFichero() throws FileNotFoundException, IOException {
+        ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream("Productos.dat"));
+        try {
+            salida.writeObject(ventas);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Fichero no encontrado");
+        } catch (IOException ex) {
+            System.out.println("Error en entrada o salida de datos");
+        }
+    }
+
+    public void leeFichero() throws FileNotFoundException, IOException, ClassNotFoundException {
+
+        try {
+            ObjectOutputStream entrada = new ObjectOutputStream(new FileOutputStream("Productos.dat"));
+            ventas = (ArrayList<Electrodomestico>) entrada.readObject();
+            entrada.close();
+
+        } catch (ClassNotFoundException ex) {
+            System.out.println("No se encuentra la clase" + ex.getMessage());
+        } catch (FileNotFoundException ex) {
+            System.out.println("Archivo no encontrado " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("Error en entrada o salida de datos " + ex.getMessage());
         }
     }
 

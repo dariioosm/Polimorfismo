ef533b9eb2c598e89d381bc87c41c34da6c96e9d
dariioosm <dariosanchez351@gmail.com>
2024-05-25 12:16:07 +0200

    correcciones menores

--- src/ejercicioFinalMayo/Electrodomestico.java
+++ src/ejercicioFinalMayo/Electrodomestico.java
@@ -6,6 +6,9 @@ public abstract class Electrodomestico {
     protected char letra;
     protected String colores;
 
+    private static final char[] CONSUMOS_VALIDOS = { 'A', 'B', 'C', 'D', 'E', 'F' };
+    private static final String[] COLORES_VALIDOS = { "blanco", "negro", "rojo", "azul", "gris" };
+
     public double getPrecioBase() {
         return precioBase;
     }
@@ -39,6 +42,10 @@ public abstract class Electrodomestico {
     }
 
     public Electrodomestico() {
+        this.precioBase = 100;
+        this.peso = 5;
+        this.letra = 'F';
+        this.colores = "blanco";
     }
 
     public Electrodomestico(double precioBase, int peso, char letra, String colores) {

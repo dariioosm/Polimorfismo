238f1d1107f7ee6dc3fbfa38c787c32156f4bea7
dariioosm <dariosanchez351@gmail.com>
2024-06-04 18:11:30 +0200

    comienzo de banquito

--- src/calificaciones/Notas.java
+++ src/calificaciones/Notas.java
@@ -3,63 +3,64 @@ package calificaciones;
 public class Notas {
 
 	public static void main(String[] args) {
-		
-		Notas nota= new Notas();
-		double notaPra=0;
-		double notaPro=0;
-		double notaT=0;
-		double notaPa=0;
-		double notaPo=0;
-		double notaTe=0;
-		String [] titulos= new String[]{"ALUMNO","PRACTICA","PROBLEMAS","TEORIA"};
-		String []alumnos= new String[]{"AAA AAA","BBB BBB","CCC CCC","FFF FFF","JJJ JJJ"};
-		double[][] notas= new double[][] {
-									{2,7,4},
-									{1,2,3},
-									{1,9,4},
-									{3,5,8},
-									{7,5,3}};
-		
-		nota.mostrarMatriz(titulos,alumnos,notas);
-		nota.notas(alumnos, notas,notaPra,notaPro,notaT, notaPa,notaPo,notaTe);
-		nota.verNotas(alumnos, notas, notaPra, notaPro, notaT);
+
+		Notas nota = new Notas();
+		String[] titulos = new String[] { "ALUMNO", "PRACTICA", "PROBLEMAS", "TEORIA" };
+		String[] alumnos = new String[] { "AAA AAA", "BBB BBB", "CCC CCC", "FFF FFF", "JJJ JJJ" };
+		double[][] notas = new double[][] {
+				{ 2, 7, 4 },
+				{ 1, 2, 3 },
+				{ 1, 9, 4 },
+				{ 3, 5, 8 },
+				{ 7, 5, 3 } };
+
+		nota.mostrarMatriz(titulos, alumnos, notas);
+		double[][] notasCalculadas = nota.calcularNotas(notas);
+		nota.verNotas(alumnos, notasCalculadas);
 	}
-	public static void mostrarMatriz(String [] titulos, String [] alumnos,double [][] notas) {  /* sacar por pantalla los datos tabulados */
-		
-		for(int i=0; i<titulos.length;i++) {
-			System.out.print(titulos[i]+"\t");
+
+	public static void mostrarMatriz(String[] titulos, String[] alumnos, double[][] notas) {
+		for (String titulo : titulos) {
+			System.out.print(titulo + "\t");
 		}
 		System.out.println();
-		for(int i=0;i<alumnos.length;i++) { 
-			System.out.print(alumnos[i]+"\t");
-			for(int j=0;j<notas[i].length;j++) {
-				System.out.print(" "+notas[i][j]+"\t"+"\t");
+		for (int i = 0; i < alumnos.length; i++) {
+			System.out.print(alumnos[i] + "\t");
+			for (int j = 0; j < notas[i].length; j++) {
+				System.out.print(notas[i][j] + "    " + "\t\t");
 			}
 			System.out.println();
 		}
-		
 	}
-	
-	public static void notas(String[]alumnos,double[][] notas,double notaPra,double notaPro,double notaT,double notaPa,double notaPo,double notaTe) {
-	
-		for(int i=0; i<notas.length;i++) {
-			for(int j=0; j<notas[0].length;j++) {
-				notaPra=notas[i][0]*0.1;
-				notaPro=notas[i][1]*0.5;
-				notaT=notas[i][2]*0.4;
+
+	public double[][] calcularNotas(double[][] notas) {
+		double[][] notasCalculadas = new double[notas.length][4];
+
+		for (int i = 0; i < notas.length; i++) {
+			double notaPra = notas[i][0] * 0.1;
+			double notaPro = notas[i][1] * 0.5;
+			double notaT = notas[i][2] * 0.4;
+			double notaFinal = notaPra + notaPro + notaT;
+
+			notasCalculadas[i][0] = notaPra;
+			notasCalculadas[i][1] = notaPro;
+			notasCalculadas[i][2] = notaT;
+			notasCalculadas[i][3] = notaFinal;
+		}
+
+		return notasCalculadas;
+	}
+
+	public void verNotas(String[] alumnos, double[][] notasCalculadas) {
+		for (int i = 0; i < alumnos.length; i++) {
+			System.out.println("\nAlumno: " + alumnos[i]);
+			System.out.println("Nota práctica: " + notasCalculadas[i][0]);
+			System.out.println("Nota problemas: " + notasCalculadas[i][1]);
+			System.out.println("Nota teoría: " + notasCalculadas[i][2]);
+			System.out.println("Nota final: " + notasCalculadas[i][3] + "\n");
+			if (notasCalculadas[i][3] < 5) {
+
 			}
 		}
-		notaPa=notaPra;
-		notaPo=notaPro;
-		notaTe=notaT;
-	}	
-	
-	public static void verNotas(String[]alumnos,double[][] notas,double notaPa,double notaPo,double notaTe){
-		double suma=0;
-		for(int i=0;i<alumnos.length;i++){
-			System.out.println("\n"+ " Alumno "+alumnos[i]+"La nota practica es "+notaPa+"La nota problemas es: "+notaPo+
-						"La nota teorica es: "+notaTe+" La nota final es: "+(suma=notaPa+notaPo+notaTe)+"\n");
-
-		}
-	}	
+	}
 }
--- src/personalSanitario/Hospitalario.java
+++ src/personalSanitario/Hospitalario.java
@@ -1,19 +1,20 @@
 package personalSanitario;
+
 import java.io.*;
-public class Hospitalario extends Sanitario implements Serializable{
+
+public class Hospitalario extends Sanitario implements Serializable {
 	private String hospital;
-	
-	
-	public Hospitalario(String apellidoNombre, String numeroRegistro, String numeroTrienios, String grupo,String hospital) {
-		super(apellidoNombre, numeroRegistro,numeroTrienios,grupo);
-		this.hospital=hospital;
-	}
-	
-	@Override
-	public String toString() {
-		return super.toString()+ "Hospitalario [hospital=" + hospital +"]";
+
+	public Hospitalario(String apellidoNombre, String numeroRegistro, int numeroTrienios, String grupo,
+			String hospital) {
+		super(apellidoNombre, numeroRegistro, numeroTrienios, grupo);
+		this.hospital = hospital;
 	}
 
+	@Override
+	public String toString() {
+		return super.toString() + "Hospitalario [hospital=" + hospital + "]";
+	}
 
 	public String getHostipal() {
 		return hospital;
@@ -23,30 +24,22 @@ public class Hospitalario extends Sanitario implements Serializable{
 		this.hospital = hospital;
 	}
 
-
-
-
-	
-
 	@Override
 	public double calculoNomina() {
-		double nomina=1700;
+		double nomina = 1700;
+		double nominaF = 0;
+		String[] prima = { "A1/A", "A2/B", "B/-", "C1/C", "C2/D" };
+		double[] dinero = { 45.29, 36.93, 32.41, 27.95, 19.02 };
 
-		String[]prima= {"A1/A","A2/B","B/-","C1/C","C2/D"};
-		double[]dinero= {45.29,36.93,32.41,27.95,19.02};
-		
-		for(int i=0;i<prima.length;i++) {
-			if(prima[i].equals(this.numeroTrienios)) {
-				double nominaF=dinero[i]*nomina;
-				
+		for (int i = 0; i < prima.length; i++) {
+			if (prima[i].equalsContent(this.numeroTrienios)) {
+				nominaF = dinero[i] * nomina;
+
 			}
 		}
 
-		
 		return nominaF;
-		
-		
+
 	}
-	
 
 }

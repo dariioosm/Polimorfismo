03785f65c72d046284206ab7c75ba443ada54569
dariioosm <dariosanchez351@gmail.com>
2024-06-06 13:49:37 +0200

    correccion de las retenciones

--- src/retenciones/Retencion.java
+++ src/retenciones/Retencion.java
@@ -1,13 +1,13 @@
 package retenciones;
 
-import javax.swing.plaf.synth.SynthGraphicsUtils;
 
 public class Retencion {
     public static void main(String[] args) {
         Retencion ret= new Retencion();
         String [] titulos=  {"COD_TRABAJADOR","SALARIO_BASE","COMPLEMENTOS","GASTOS"};
         String[] codigos={"1AAA","2BBB","6CCC","12RR","32HH"};
-        int []suma;
+        int []suma={0,0,0,0,0};
+        double[]fin={0,0,0,0,0};
         int[][]numeros={
                         {2000,700,40},
                         {1000,200,30},
@@ -16,6 +16,8 @@ public class Retencion {
                         {700,50,30},
                         };
         ret.imprimeMatriz(titulos,codigos ,numeros);
+        ret.retencion(codigos, numeros, suma);
+        ret.imprimeRet(codigos, suma,fin);
     }
    public void imprimeMatriz(String [] titulos,String []codigos,int[][]numeros){
     for(int i=0; i<titulos.length;i++) {
@@ -32,10 +34,6 @@ public class Retencion {
 
     }
     public void retencion(String[]codigos,int[][]numeros,int []suma){
-        int[]min={0,1000,1300,1800};
-        int []max={999,1299,1799,5000};
-        double[] ret={0.03,0.07,0.12,0.21};
-        double fin=0;
         for (int i=0; i<numeros.length;i++){
             for(int j=0; j<numeros[0].length;j++){
                 suma[i]+=numeros[i][j];
@@ -43,7 +41,16 @@ public class Retencion {
         }
     }
 
-    public void imprimeRet(){
+    public void imprimeRet(String[] codigos,int []suma,double[]fin){
+        int[]min={0,1000,1300,1800};
+        int []max={999,1299,1799,5000};
+        double[] ret={0.03,0.07,0.12,0.21};
+        for(int i=0; i<suma.length;i++){
+            if(suma[i]>=min[i]&&suma[i]<=max[i]){
+                fin[i]=ret[i]*suma[i];
+            }
+            System.out.println("la retencion aplicada al trabajador "+codigos[i]+" es "+fin[i]);
+        }
 
     }
 

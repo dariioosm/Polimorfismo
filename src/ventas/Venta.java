c5e6adcb78649845e094c65b059c242f978b594a
dariioosm <dariosanchez351@gmail.com>
2024-06-06 13:57:55 +0200

    comienzo del problema de ventas

---
+++ src/ventas/Venta.java
@@ -0,0 +1,57 @@
+package ventas;
+
+public abstract class Venta {
+    protected String fechaHora;
+    protected String evento;
+
+
+
+    
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+
+
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+
+
+
+    public String getEvento() {
+        return evento;
+    }
+
+
+
+
+    public void setEvento(String evento) {
+        this.evento = evento;
+    }
+
+
+    public Venta(){
+        
+    }
+
+    public Venta(String fechaHora, String evento) {
+        this.fechaHora = fechaHora;
+        this.evento = evento;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "Venta [fechaHora=" + fechaHora + ", evento=" + evento + "]";
+    }
+
+
+
+
+    public abstract double calcularIngresoVenta();
+}
